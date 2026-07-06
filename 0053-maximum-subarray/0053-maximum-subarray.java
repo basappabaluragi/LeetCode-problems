e964b5360bd8d4f2ddced155c6170677cc8314d5
Basappa Mallappa Baluragi <basappabaluragi341@gmail.com>
2026-07-06 17:24:46 +0530

    Time: 1 ms (99.96%), Space: 77 MB (82.21%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+       int max=nums[0];
+       int currentsum=0;
+       for(int num:nums){
+        currentsum=currentsum+num;
+        if(currentsum>max)max=currentsum;
+        if(currentsum<0)currentsum=0;
+       }
+       return max; 
+    }
+}
